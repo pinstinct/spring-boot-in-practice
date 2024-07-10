e003f8c0f48f8a9322a8fd50cc8d9a6e51d7829e
pinstinct <contact@limhm.dev>
2024-07-10 14:09:05 +0900

    SpringApplication 클래스의 setDefaultProperties() 메서드 사용

--- src/main/java/com/limhm/maven/project/Application.java
+++ src/main/java/com/limhm/maven/project/Application.java
@@ -1,5 +1,6 @@
 package com.limhm.maven.project;
 
+import java.util.Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
+    Properties properties = new Properties();
+    // ConfigDataLocationNotFoundException 예외를 무시하고, 애플리케이션 시동 작업을 계속 진행
+    properties.setProperty("spring.config.on-not-found", "ignore");
+
     //SpringApplication 인스턴스를 직접 생성해서 웹 애플리케이션 타입을 서블릿으로 지정
     SpringApplication application = new SpringApplication(Application.class);
     application.setWebApplicationType(WebApplicationType.SERVLET);
+    application.setDefaultProperties(properties);
     application.run(args);
   }
 

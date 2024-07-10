415356ff9fab956fd78abaa6a84446e839a6772e
pinstinct <contact@limhm.dev>
2024-07-10 10:36:27 +0900

    메인 클래스에 SpringApplication 인스턴스 직접 생성

--- src/main/java/com/limhm/maven/project/Application.java
+++ src/main/java/com/limhm/maven/project/Application.java
@@ -1,13 +1,17 @@
 package com.limhm.maven.project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    //SpringApplication 인스턴스를 직접 생성해서 웹 애플리케이션 타입을 서블릿으로 지정
+    SpringApplication application = new SpringApplication(Application.class);
+    application.setWebApplicationType(WebApplicationType.SERVLET);
+    application.run(args);
   }
 
 }

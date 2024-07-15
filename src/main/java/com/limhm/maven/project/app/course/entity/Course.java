e95d68446f19ffa0a22a09bca6be42c4fbf504db
pinstinct <contact@limhm.dev>
2024-07-15 10:28:08 +0900

    Course 엔티티에 bean validation 추가 및 테스트 코드 작성

---
+++ src/main/java/com/limhm/maven/project/app/course/entity/Course.java
@@ -0,0 +1,50 @@
+package com.limhm.maven.project.app.course.entity;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+public class Course {
+
+  private long id;
+
+  private String name;
+
+  private String category;
+
+  @Min(value = 1, message = "A course should have a minimum of 1 rating")
+  @Max(value = 5, message = "A course should have a maximum of 5 rating")
+  private int rating;
+
+  private String description;
+
+  protected Course() {
+  }
+
+  protected Course(long id, String name, String category, int rating, String description) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.rating = rating;
+    this.description = description;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getCategory() {
+    return category;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+}
---
+++ src/test/java/com/limhm/maven/project/app/course/entity/CourseTest.java
@@ -0,0 +1,27 @@
+package com.limhm.maven.project.app.course.entity;
+
+import com.limhm.maven.project.ApplicationTests;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CourseTest extends ApplicationTests {
+
+  private static final Logger logger = LoggerFactory.getLogger(CourseTest.class);
+
+  @Test
+  public void validateRating() throws Exception {
+    Course course = new Course(1, "CS50", "Computer", 0, "컴퓨터 기초 수업");
+    Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    // Course 객체에 정의된 모든 제약 사항 준수 여부를 검증하고 위반 사항을 모아서 반환
+    Set<ConstraintViolation<Course>> violations = validator.validate(course);
+    violations.forEach(courseConstraintViolation -> logger.error(
+        "A constraint violation has occurred. Violation details: [{}].",
+        courseConstraintViolation));
+  }
+}

49b99b8499bce3c3a44180a0ab694bae4d8b1775
pinstinct <contact@limhm.dev>
2024-07-18 15:21:48 +0900

    관계형 데이터베이스에서 스프링 데이터 JPA를 사용한 커스텀 쿼리 메서드 정의

--- src/main/java/com/limhm/maven/project/app/model/course/repository/CourseRepository.java
+++ src/main/java/com/limhm/maven/project/app/model/course/repository/CourseRepository.java
@@ -1,10 +1,32 @@
 package com.limhm.maven.project.app.model.course.repository;
 
 import com.limhm.maven.project.app.model.course.entity.Course;
+import java.util.stream.Stream;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository  // 스프링 리포지터리로 사용된다는 것을 알려줌, 예외 변환(JPA 구현체마다 달라지는 예외가 아니라 DataAccessException 만 처리하면 됨)
 public interface CourseRepository extends CrudRepository<Course, Long> {
   // 비어 있더라도 스프링 데이터 JPA 가 런타임에 인터페이스 구현체를 자동으로 생성
+
+  // 카테고리 기준으로 모든 과정 정보 목록 조회
+  Iterable<Course> findAllByCategory(String category);
+
+  // 카테고리 기준으로 모든 과정 정보 목록 조회
+  Stream<Course> streamAllByCategory(String category);
+
+  // 카테고리 기준으로 모든 과정 정보 목록을 조회해서 이름 기준으로 오름차순 정렬
+  Iterable<Course> findAllByCategoryOrderByName(String category);
+
+  // 이름으로 과정을 조회해서 있으면 true, 없으면 false 반환
+  boolean existsByName(String name);
+
+  // 카테고리 기준으로 조회한 과정 정보의 개수 반환
+  long countByCategory(String category);
+
+  // 이름이 같거나 카테고리가 같은 과정 목록 조회
+  Iterable<Course> findByNameOrCategory(String name, String category);
+
+  // 이름이 주어진 문자열로 시작하는 과정 목록 조회
+  Iterable<Course> findByNameStartsWith(String name);
 }
--- src/test/java/com/limhm/maven/project/app/model/course/repository/CourseRepositoryTest.java
+++ src/test/java/com/limhm/maven/project/app/model/course/repository/CourseRepositoryTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.limhm.maven.project.ApplicationTests;
 import com.limhm.maven.project.app.model.course.entity.Course;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,6 +15,11 @@ public class CourseRepositoryTest extends ApplicationTests {
 
   @Autowired
   private CourseRepository courseRepository;
+
+  @BeforeAll
+  public static void setUp(@Autowired CourseRepository courseRepository) {
+    courseRepository.deleteAll();
+  }
 
   @Test
   public void givenCreateCourseWhenLoadTheCourseThenExpectSameCourse() {
@@ -48,5 +56,42 @@ public class CourseRepositoryTest extends ApplicationTests {
     assertThat(courseRepository.findById(savedCourse.getId()).get()).isEqualTo(course);
     courseRepository.delete(course);
     assertThat(courseRepository.findById(savedCourse.getId()).isPresent()).isFalse();
+  }
+
+  @Test
+  public void givenCreateCoursesWhenLoadTheCourseThenExpectSameCourses() {
+    courseRepository.saveAll(getCourses());
+    assertThat(courseRepository.findAllByCategory("Spring")).hasSize(3);
+    assertThat(courseRepository.existsByName("JavaScript for All")).isTrue();
+    assertThat(courseRepository.existsByName("Mastering JavaScript")).isFalse();
+    assertThat(courseRepository.countByCategory("Python")).isEqualTo(2);
+    assertThat(courseRepository.findByNameStartsWith("Getting Started")).hasSize(3);
+  }
+
+  private List<Course> getCourses() {
+    Course rapidSpringBootCourse = new Course("Rapid Spring Boot Application Development", "Spring",
+        4, "Spring Boot gives all the power of the Spring Framework without all fo the complexity");
+
+    Course springSecurityDslCourse = new Course("Getting Started with Spring Security DSL",
+        "Spring", 5, "Learn Spring Security DsL in easy steps");
+
+    Course springCloudKubernetesCourse = new Course("Getting Started with Spring Cloud Kubernetes",
+        "Spring", 3, "Master Spring Boot application deployment with Kubernetes");
+
+    Course rapidPythonCourse = new Course("Getting Started with Python", "Python", 5,
+        "Learn Python concepts in easy steps");
+
+    Course gameDevelopmentWithPython = new Course("Game Development with Python", "Python", 3,
+        "Learn Python by developing 10 wonderful games");
+
+    Course javascriptForAll = new Course("JavaScript for All", "JavaScript", 4,
+        "Learn basic JavaScript syntax that can apply to anywhere");
+
+    Course javascriptCompleteGuide = new Course("JavaScript Complete Guide", "JavaScript", 5,
+        "Master JavaScript with Core Concepts and Web Development");
+
+    return Arrays.asList(rapidSpringBootCourse, springSecurityDslCourse,
+        springCloudKubernetesCourse, rapidPythonCourse, gameDevelopmentWithPython, javascriptForAll,
+        javascriptCompleteGuide);
   }
 }
